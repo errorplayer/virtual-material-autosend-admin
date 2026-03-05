4b84b7c9985961e05d58c2a71136d618a95f297e
linzeyang <zjnd_zy@outlook.com>
2026-03-05 21:51:18 +0800

    for vercel build deploy modifying frontpage

--- app/page.tsx
+++ app/page.tsx
@@ -13,34 +13,30 @@ import { useEffect, useState } from 'react';
 import { supabase } from '../lib/supabase';
 
 export default function Home() {
-  const [message, setMessage] = useState('加载中...');
+  // const [message, setMessage] = useState('加载中...');
 
-  useEffect(() => {
-    async function testConnection() {
-      const { data, error } = await supabase.from('messages').select('*');
+  // useEffect(() => {
+  //   async function testConnection() {
+  //     const { data, error } = await supabase.from('messages').select('*');
 
-      if (error) {
-        setMessage('连接成功！但还没有 messages 表：' + error.message);
-      } else {
-        setMessage('✅ Next.js + Supabase 今天是2026年3月2日！数据：' + JSON.stringify(data));
-      }
-    }
+  //     if (error) {
+  //       setMessage('连接成功！但还没有 messages 表：' + error.message);
+  //     } else {
+  //       setMessage('✅ Next.js + Supabase 今天是2026年3月2日！数据：' + JSON.stringify(data));
+  //     }
+  //   }
 
-    testConnection();
-  }, []);
+  //   testConnection();
+  // }, []);
 
   return (
-       <main >
-            <div>
-                <h1 >Next.js + Supabase 测试</h1>
-             <p>{message}</p>
-            </div>
+    <main >
+      <div>
+        <h1>商品管理系统</h1>
+        <h2>loading...</h2>
 
-            {/* <div className="bg-red-500">Test</div> */}
+      </div>
 
-             
-      {/* <h1 className="text-2xl font-bold text-red-500">Tailwind测试</h1>
-      <p className="mt-4 text-blue-400">如果文字是红色/蓝色，说明Tailwind生效了</p> */}
-        </main>
-    );
+    </main>
+  );
 }
